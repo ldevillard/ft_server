9e3102c99d64f0b38b79a26b2f8ef1c401be9d38
ldevillard <legoexpliation@gmail.com>
2021-01-08 14:12:26 +0100

    wordpress fixed

--- srcs/start.sh
+++ srcs/start.sh
@@ -6,37 +6,42 @@
 #    By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/07 12:34:12 by ldevilla          #+#    #+#              #
-#    Updated: 2021/01/08 13:10:26 by ldevilla         ###   ########lyon.fr    #
+#    Updated: 2021/01/08 13:37:00 by ldevilla         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 #NGINX
 cp /srcs/default /etc/nginx/sites-available/default
 
+#MYSQL
+service mysql start
+mysql -u root -e "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}'; 
+GRANT ALL PRIVILEGES ON * . * TO '${USER}'@'localhost' WITH GRANT OPTION; 
+FLUSH PRIVILEGES;"
+
+mysql -u root -e "CREATE DATABASE wordpress;"
+
 #PHP MYADMIN
 cd var/www/html
 mkdir phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz > /dev/null
 cp -r phpMyAdmin-4.9.0.1-all-languages/* phpmyadmin
 rm -rf phpMyAdmin-4.9.0.1-all-languages phpMyAdmin-4.9.0.1-all-languages.tar.gz
 cp /srcs/config.inc.php ./phpmyadmin
 
-#MYSQL
-service mysql start
-echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-echo "GRANT ALL ON wordpress.* TO '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
 #WORDPRESS
 wget https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
-cp /srcs/wp-config.php /var/www/html
-sed 's/username_here/'${USER}'/g' wp-config.php
-sed 's/password_here/'${PASSWORD}'/g' wp-config.php
+tar -xvzf latest.tar.gz > /dev/null
+rm -rf latest.tar.gz
+cp /srcs/wp-config.php /var/www/html/wordpress
+cd /var/www/html/wordpress
+sed -i 's/username_here/'${USER}'/g' wp-config.php
+sed -i 's/password_here/'${PASSWORD}'/g' wp-config.php
 
 service nginx start
+service mysql restart
 service php7.3-fpm start
 #sleep infinity
-#bash
-tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
+bash
+#tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
